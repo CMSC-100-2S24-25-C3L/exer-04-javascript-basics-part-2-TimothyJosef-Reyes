7147fe11f9bf1f22ca836cbd630420637e72a191
TJ Reyes <tareyes1@up.edu.ph>
2025-02-27 14:25:13 +0800

    feat: completed the addAccount function

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import { v4 as uuidv4 } from 'uuid';
 import validator from 'validator';
+var fs = require('fs');
 
 function generateUniqueID(first_name, last_name){
     if(typeof first_name != 'string' || typeof last_name != 'string'){
@@ -23,10 +24,30 @@ function generateUniqueID(first_name, last_name){
 }
 
 function addAccount(user_info){
-    if(user_info.lenght != 4){
+    if(user_info.length != 4){
         return false;
     }
 
+    if((typeof user_info[0] != "string" && user_info[0].length == 0) ||
+    (typeof user_info[1] != "string" && user_info[1].length == 0) ||
+    (typeof user_info[2] != "string" && user_info[2].length == 0)){
+        return false;
+    }
+
+    if(Number(user_info[3]) < 18){
+        return false;
+    }
+
+    if(!validator.isEmail(user_info[2])){
+        return false;
+    }
+
+    let uniqueID = generateUniqueID(user_info[0], user_info[1]);
+
+    console.log(user_info[0]+","+user_info[1]+","+user_info[2]+","+user_info[3]+","+uniqueID);
+
+    return true;
 }
 
 console.log(generateUniqueID("Timothy", "Reyes"));
+console.log(addAccount(["Timothy", "Reyes", "tareyes1@gmail.com", 18]));
