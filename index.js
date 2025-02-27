4d9a2bc67195869200f6326b3fa7ce5d4cb6af32
TJ Reyes <tareyes1@up.edu.ph>
2025-02-27 14:05:06 +0800

    feat: completed generateUniqueID function and added gitignore

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
+import { v4 as uuidv4 } from 'uuid';
+import validator from 'validator';
 
+function generateUniqueID(first_name, last_name){
+    if(typeof first_name != 'string' || typeof last_name != 'string'){
+        first_name = String(first_name);
+        last_name = String(last_name);
+    }
+
+    first_name = first_name.toLowerCase();
+    last_name = last_name.toLowerCase();
+
+    let uniqueID = "";
+    let id = uuidv4();
+    
+    id = String(id).substr(0,8);
+    
+    uniqueID = uniqueID + first_name[0];
+    uniqueID = uniqueID + last_name;
+    uniqueID = uniqueID + id;
+
+    return uniqueID;
+}
+
+function addAccount(user_info){
+    if(user_info.lenght != 4){
+        return false;
+    }
+
+}
+
+console.log(generateUniqueID("Timothy", "Reyes"));
